b1547dfa3c483a376c5fabebf123474f71487edf
matchy233 <matchy9808@gmail.com>
2020-02-01 00:03:33 -0500

    Add new plotting functions in .py file and generate new plots

--- hw/hw2/hw2_plot.py
+++ hw/hw2/hw2_plot.py
@@ -16,7 +16,7 @@ def cobwebbing(x0, fun, output_x, output_y, curr_layer=0, max_layer=50):
         return
     cobwebbing(fun(x0), fun, output_x, output_y, curr_layer+1, max_layer)
 
-def plot_cobwebbing(x0, fun, x_bar, title):
+def plot_cobwebbing(x0, fun, x_bar, title, max_layer=20):
     """ Use the cobwebbing method to plot a higher-order difference equation
     Args:
         x0: initial value
@@ -25,21 +25,45 @@ def plot_cobwebbing(x0, fun, x_bar, title):
         title: the title of the figure to save
     """
     x = []; y = []
-    cobwebbing(x0, fun, x, y, max_layer=20)
+    cobwebbing(x0, fun, x, y, max_layer=max_layer)
     plt.figure()
     plt.plot(x, y, '--o')
-    plt.xticks(range(0, 21))
+    plt.xticks(range(0, max_layer+1, max_layer//20))
     plt.xlabel("$x_n$")
     plt.ylabel("$x_{n+1}$")
-    plt.axhline(x_bar, color="red")
+    plt.axhline(x_bar, color="magenta")
     file_name = rf"./fig/{title}.pdf"
     if os.path.isfile(file_name):
         os.remove(file_name)
     plt.savefig(file_name)
 
+def plot_cobwebbing_2a(x0, x_bar, max_layer=150):
+    """ Use the cobwebbing method to plot a higher-order difference equation
+
+    This is the version designed especially for exploring the boundary r value
+    of problem 2(a)
+    """
+    r_vals = [-1, -0.95, -0.9, -0.5, 0.5, 0.7, 0.8, 0.9, 0.95, 0.98, 1]
+    def fun(r, x):
+        return r*x*(1-x)
+    data = []
+    for i in range(len(r_vals)):
+        data.append(([], []))
+        cobwebbing(x0, lambda x: fun(r_vals[i], x),
+                data[i][0], data[i][1], max_layer=max_layer)
+    plt.figure()
+    for x, y in data:
+        plt.plot(x, y, 'o', markersize=2, lw=0.5)
+    plt.legend([f"r = {r}" for r in r_vals], loc='lower right')
+    plt.xlabel("$x_n$")
+    plt.ylabel("$x_{n+1}$")
+    plt.axhline(x_bar, color="magenta",zorder=5)
+    plt.show()
+
 def plot_logis_lambda(r, K):
     x = np.arange(2*K)
     y = list(map(lambda x: pow(np.e, r*(1-x/K)), x))
+    plt.figure()
     plt.plot(x, y)
     plt.xlabel("$N$")
     plt.ylabel(r"$\lambda$")
@@ -49,18 +73,31 @@ def plot_logis_lambda(r, K):
     y_mid = ytick[len(ytick)//2+1]
     x_int = xtick[1] - xtick[0]
     del fig, xtick, ytick
-    plt.text(K+0.1*x_int, y_mid, 'N = K', 
+    plt.text(K+0.1*x_int, y_mid, 'N = K',
             rotation=-90, fontsize=12, color="red")
     plt.axhline(1, xmax=0.5, linestyle='--', color='r', label=r"$\lambda = 1$")
     plt.plot(K, 1, 'ro')
-    plt.show()
+    plt.savefig(r"./fig/fig4(a).pdf")
 
 def main():
-    plot_cobwebbing(0.001, lambda x: 2*x*(1-x), 0, "fig2(a)")
+    plot_cobwebbing(
+        0.01,
+        lambda x: 2*x*(1-x),
+        0,
+        "fig2(a)(1)"
+    )
+    plot_cobwebbing(
+        0.01,
+        lambda x: 0.5*x*(1-x),
+        0,
+        "fig2(a)(2)"
+    )
     plot_cobwebbing(
         1.6,
         lambda x: -(x**2)*(1-x),
-        (1+np.sqrt(5))/2, "fig2(b)")
+        (1+np.sqrt(5))/2,
+        "fig2(b)"
+    )
     plot_cobwebbing(
         0.4,
         lambda x: 1/(2+x),
@@ -73,6 +110,7 @@ def main():
         np.e**0.5,
         "fig2(d)"
     )
+    plot_logis_lambda(2, 50)
 
 if __name__ == "__main__":
-    plot_logis_lambda(2, 50)
+    main()
