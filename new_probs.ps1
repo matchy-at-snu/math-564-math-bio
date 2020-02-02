7f6692944a6a43fdf0d8a2e156de49cb3fb09a3b
matchy233 <matchy9808@gmail.com>
2020-02-01 23:59:31 -0500

    Refactor hw1 file structure

--- new_probs.ps1
+++ new_probs.ps1
@@ -1,7 +1,7 @@
-$nums = 4, 8, 11, 16, 17
+$nums = 1, 2, 3, 4, 6, 8, 9, 10, 16, 17, 18, 20
 
 foreach ($i in $nums) {
     $fileName = 'prob' + $i + '.tex'
-    $newPath= Join-Path -path 'sections/hw2/' -childpath $fileName
+    $newPath= Join-Path -path 'hw1/sections/' -childpath $fileName
     New-Item $newPath -type file
 }
